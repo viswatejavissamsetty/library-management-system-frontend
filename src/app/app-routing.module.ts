7ad789868ce7d3e7d50cd324b70a17780cf4a8cb
viswatejavissamsetty <viswateja994@gmail.com>
2022-01-21 12:31:04 +0530

    server mechanism added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login', 
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule) 
+  },
+  {
+    path: 'register', 
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
